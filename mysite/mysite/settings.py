3a5cb907cd2ec2cd5d228bc142ca6f192586d92c
Kubanycheva <lkybanucheva@gmail.com>
2025-06-09 15:31:51 +0600

    al

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -171,5 +171,6 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://halal-proje.vercel.app"
 ]
 CORS_ALLOW_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
